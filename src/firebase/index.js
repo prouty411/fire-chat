57679e7a0133e9198221a043e0b0914e5c4aff40
Sean Prouty <prouty_411@hotmail.com>
2018-09-07 16:14:35 -0700

    connected to db

---
+++ src/firebase/index.js
@@ -0,0 +1,9 @@
+import firebase from 'firebase/app';
+import 'firebase/database';
+import config from '../config'
+
+
+firebase.initializeApp(config.firebase);
+
+export const db = firebase.database();
+
